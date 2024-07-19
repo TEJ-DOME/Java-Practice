1e16f9a5fabb1c98daa2bb563aaa2131c16c0041
TEJ-DOME <tejvarun08@gmail.com>
2024-07-19 22:40:00 +0530

    Add files via upload

---
+++ Myself.java
@@ -0,0 +1,19 @@
+package labprgms;
+
+public class Myself {
+	public static void main(String[] args) {
+		// Writing about myself
+		System.out.println("My name is Kattola Tejvarun.");
+		System.out.println("I am from Hyderabad which is located in Telangana State.");
+		System.out.println("I am studying MCA 1st year at Avanthi PG college. ");
+		System.out.println("I have completed my Graduation B.Com(Computers) from Vasundara Degree College.");
+		System.out.println("I have Profesional Experience of 2.5 yrs in Non-IT Field.");
+		System.out.println("I am Confident and Optimistic.");
+		System.out.println("Always in serach of learning in all Aspects.");
+		System.out.println("Failures are Fruits... Get the fruit and taste the Success.");
+		System.out.println("My Hobbies are playing mobile games.");
+		System.out.println("My favourite movie is Ninu Kori");
+		System.out.println("Be Cool :)");
+
+	}
+}
